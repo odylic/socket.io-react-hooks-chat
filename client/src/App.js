9b7aac2558f2489b5aa9031d9ed8b090748ff144
Jimmy <odylic.lin@gmail.com>
2021-11-24 14:12:50 -0800

    chatroom

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Home/Home.jsx";
-// import Chatroom from "./Chatroom/Chatroom";
+import Chatroom from "./Chatroom/Chatroom.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route exact path="/:roomId" component={Chatroom}/> */}
+        <Route exact path="/:roomId" element={<Chatroom />} />
       </Routes>
     </Router>
   );
---
+++ client/src/Chatroom/Chatroom.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Chatroom.css";
+
+const Chatroom = (props) => {
+  // const { roomId } = props.match.params;
+
+
+  return (
+    <div className="chat-room-container">
+      {/* <h1 className="room-name">Room: {roomId}</h1> */}
+      <div className="messages-container">Test</div>
+    </div>
+  );
+};
+export default Chatroom;
