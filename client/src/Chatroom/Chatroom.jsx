9edea958597edfe2aacfe30270dcc2f1a99b202a
Jimmy <odylic.lin@gmail.com>
2021-11-25 10:50:38 -0800

    useParams

--- client/src/Chatroom/Chatroom.jsx
+++ client/src/Chatroom/Chatroom.jsx
@@ -1,16 +1,43 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import "./Chatroom.css";
 
 import useChat from "../useChat";
 
 const Chatroom = (props) => {
-  // const { roomId } = props.match.params;
-  // const { messages, sendMessage } = useChat(roomId);
+  // gets the roomId for the URL params
+  const { roomId } = useParams();
+  const { messages, sendMessage } = useChat(roomId);
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleNewMessageChange = (e) => {
+    setNewMessage(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    sendMessage(newMessage);
+    setNewMessage("");
+  };
 
   return (
     <div className="chat-room-container">
-      {/* <h1 className="room-name">Room: {roomId}</h1> */}
-      <div className="messages-container">Test</div>
+      <h1 className="room-name">Room: {roomId}</h1>
+      <div className="messages-container">
+        <ol className="messages-list">
+          {messages.map((message, i) => {
+            <li
+              key={i}
+              // will show the css conditionally for my-message or received message
+              className={`message-item ${
+                message.ownedByCurrentUser ? "my-message" : "received-message"
+              }`}
+            >
+              {message.body}
+            </li>;
+          })}
+        </ol>
+      </div>
+      
     </div>
   );
 };
